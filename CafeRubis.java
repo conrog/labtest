3b5bbf04ede605985a222e5c0a64f68f731311de
Conor Rogers <c17730535@mydit.ie>
2019-03-06 19:19:34 +0000

    Creating Repo

---
+++ CafeRubis.java
@@ -0,0 +1,17 @@
+public class CafeRubis extends PApplet
+{
+    public void settings()
+    {
+        size(800,600);
+    }
+
+    public void setup()
+    {
+
+    }
+
+    public void draw()
+    {
+
+    }
+}
